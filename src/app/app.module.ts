232993eb41788133b8d0413b48b6e55530b02c06
zk-v <zakodos.vesna@gmail.com>
2019-11-05 13:49:03 +0100

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
 import { InputTaskComponent } from './input-task/input-task.component';
-import { AllTasksComponent } from './all-tasks/all-tasks.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, InputTaskComponent, AllTasksComponent ],
+  declarations: [ AppComponent, InputTaskComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
